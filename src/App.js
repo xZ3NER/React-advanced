dadef07deff967e84644b0e8baa34980a4eb96d3
Peng You <ypenggg11@gmail.com>
2023-02-23 21:13:36 +0000

    Side effect hook added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
+/* 6.- Side Effect & Local storage */
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("isUserLogged") === "true") {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isUserLogged", "true");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isUserLogged");
     setIsLoggedIn(false);
   };
 
